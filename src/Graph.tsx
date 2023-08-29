85725c5c7466e8100a2abfef77c9278494eba402
wilsonk121 <wilsonlj@connect.hku.hk>
2023-08-29 13:26:37 +0800

    debugged 2

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -39,7 +39,6 @@ class Graph extends Component<IProps, {}> {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
       elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
       elem.setAttribute('row-pivots', '["timestamp"]');
       elem.setAttribute('columns', '["top_ask_price"]');
       elem.setAttribute('aggregates', JSON.stringify({
